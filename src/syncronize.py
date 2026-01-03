57b8fd3b6eb9d67aa2f768600c46a70248123e90
AlbertSolomon <solomonaalbert@gmail.com>
2026-01-03 10:36:27 +0200

    thau shall not crush

---
+++ src/syncronize.py
@@ -0,0 +1,24 @@
+from lang import get_ot_data, path, t_path, settings_path, rules_path
+from ot import OT
+from openpyxl import Workbook, load_workbook
+
+workbook = load_workbook(path)
+workbook.active
+
+def sync_json_data():
+   # might use a helper function but who knows 
+   ot_data = get_ot_data()
+   #print(ot_data)
+   #print(type(ot_data))
+   for key in ot_data:
+       # print(ot_data[key])
+    try:
+         print(f"{ key }:",OT(workbook, key).finder())
+    except KeyError:
+        print(f"{key} does not exist in the workbook")
+           
+   # sheetname, DATE, DAY, PURPOSE (depends with the posistion ),START, FINISH, TOTAL HOURS, RATE(if its not saturday or sunday rate = 1.00)
+       #if self.finder(key):
+       #    sheet = self.workbook[key]
+
+sync_json_data()
