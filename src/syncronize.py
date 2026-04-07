0500e5ba63b8a8c90ad4930213aa36b657740e43
AlbertSolomon <solomonaalbert@gmail.com>
2026-04-07 20:18:49 +0200

    openpyxl not playing nice with me

--- src/syncronize.py
+++ src/syncronize.py
@@ -28,22 +28,23 @@ def sync_json_data():
                 print(ot_data[key])
                 data_list = ot_data[key]
                 for _, indx in enumerate(data_list):
-                    #print(data_list[_])
+                    #print(data_list[_]
                     record_dict = data_list[_]
                     #print(temp_workbook.insert_point_coordinate())
                     #print(start_point)
                     #print(f"index ::: { indx }",record_dict.get("date"))
                     
-                    temp_workbook.insert_cell_value("A" + start_point, record_dict.get("date"))
-                    temp_workbook.insert_cell_value("B" + start_point, record_dict.get("holiday"))
-                    temp_workbook.insert_cell_value("F" + start_point, record_dict.get("start"))
-                    temp_workbook.insert_cell_value("G" + start_point, record_dict.get("finish"))
-                    temp_workbook.insert_row()
-                    print(record_dict.get("date"))
-                    print(record_dict.get("holiday"))
-                    print(record_dict.get("start"))
-                    print(record_dict.get("finish"))
+                    temp_workbook.insert_cell_value("A" + str(int(start_point) + _), record_dict.get("date"))
+                    temp_workbook.insert_cell_value("B" + str(int(start_point) + _), record_dict.get("holiday"))
+                    temp_workbook.insert_cell_value("F" + str(int(start_point) + _), record_dict.get("start"))
+                    temp_workbook.insert_cell_value("G" + str(int(start_point) + _), record_dict.get("finish"))
+                    
+                    print(f"{ "A" + str(int(start_point) + _) } : { record_dict.get("date") }")
+                    print(f"{ "B" + str(int(start_point) + _) } : { record_dict.get("holiday") }")
+                    print(f"{ "F" + str(int(start_point) + _) } : { record_dict.get("start") }")
+                    print(f"{ "G" + str(int(start_point) + _) } : { record_dict.get("finish") }")
                     print("\n") # insert row here 
+                    temp_workbook.insert_row()
             workbook.save(f"./temp/{ overtime_month }.xlsx")
         except KeyError:
             print(f"{key} does not exist in the workbook")
